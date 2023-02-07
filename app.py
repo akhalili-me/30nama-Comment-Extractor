460c54146c645f265f31462a324f662db90cb8ba
Amir Khalili <akhalili.dev@gmail.com>
2023-02-07 18:00:29 +0330

    Fixed the Exceptions on clicking on more button and fixed the url maker function

--- app.py
+++ app.py
@@ -1,53 +1,35 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
 from src import *
 
-
+#Build movie/serie URL
 name = input("Please Enter the movie's/serie's name: ")
-url = 'https://30nama.com/search?q=' + name
+url = get_movie_url(name)
+
 
 # Search for movie/serie on 30nama
-browser = webdriver_option('chrome')
+browser = webdriver_option('firefox')
 response = url_to_bs4(url,browser)
 search_result = get_search_result(response)
 
-space(2)
+space(1)
+
 
 # choose between searched movies/series
 print('----- Please choose your desired movie/serie link -----')
 for idx,result in enumerate(search_result):
     print(str(idx) + ': ' + result)
 selected = int(input('Your choice:'))
-
 selected_url = 'https://30nama.com' + search_result[selected] + '?section=comments'
 
 
-#click on more to show all comments
-click_more_comments(browser,selected_url)
+
+#click on more button to show all comments
+click_more_comments(browser,selected_url,2)
 
 
+#Extracting comments
+comments = extract_comments(browser.page_source)
 
-response = browser.page_source
-res = extract_comments(browser.page_source)
-k
-
-for item in res:
+#Printing the result 
+for item in comments:
     print('-----')
     print(item)
-
-   
-
-
-
-
-
-# result = []
-# soup = BeautifulSoup(response, 'html.parser')
-# for comment in soup.find_all('p'):
-#     if comment.get('data-v-0b757e80') == '':
-#         print(comment.contents)
-#         # result.append(comment.contents)
-
-
-
-# print(response)
--- src.py
+++ src.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import requests
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException,NoSuchElementException
+
+def get_movie_url(name):
+    name = name.replace(' ', '+')
+    url = 'https://30nama.com/search?q=' + name + '+'
+    return url
 
 def webdriver_option(browser):
 
@@ -10,7 +15,7 @@ def webdriver_option(browser):
         'chrome': webdriver.ChromeOptions,
         'edge': webdriver.EdgeOptions
     }
-
+    
     option_class = options.get(browser, webdriver.FirefoxOptions)
     options = option_class()
     options.add_argument("--headless")
@@ -22,9 +27,6 @@ def webdriver_option(browser):
     
     driver = driver_map.get(browser, webdriver.Firefox)(options=options)
     return driver
-
-
-
 
 def url_to_bs4(url,browser):
     browser.get(url)
@@ -42,13 +44,17 @@ def get_search_result(response):
 
     return result 
 
-
-
-def click_more_comments(browser,url):
+def click_more_comments(browser,url,count=1):
     browser.get(url)
-    element = browser.find_element(By.XPATH,"//div[@data-v-75ef1040='' and @data-v-7237fd26='']")
-    browser.execute_script("arguments[0].scrollIntoView();", element)
-    browser.execute_script("arguments[0].click();", element)
+    for i in range(count):
+        try:
+            element = browser.find_element(By.XPATH,"//div[@data-v-75ef1040='' and @data-v-7237fd26='']")
+            browser.execute_script("arguments[0].scrollIntoView();", element)
+            browser.execute_script("arguments[0].click();", element)
+        except TimeoutException:
+            break
+        except NoSuchElementException:
+            break
 
 def check_comment(comment):
     return comment not in ['برای امتیاز‌دهی باید وارد شوید','۰','۱','۲','۳','۴','۵','۶','۷','۸','۹']
@@ -56,10 +62,11 @@ def check_comment(comment):
 def extract_comments(response):
     result = []
     soup = BeautifulSoup(response, 'html.parser')
-    for comment in soup.find_all('p', attrs={'data-v-0b757e80': ''}):
-        str_comment = str(comment.contents[0]).strip()
-        if check_comment(str_comment):
-            result.append(str_comment)
+    for comment in soup.find_all('p'):
+        if comment.get('data-v-0b757e80') == '':
+            str_comment = str(comment.contents[0]).strip()
+            if check_comment(str_comment):
+                result.append(str_comment)
+    
     return result
-
 
